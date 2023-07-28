b1bfb0f58e904c0b6dba71ddebc23df6d62db578
Szymon Osiński <szymek16s@gmail.com>
2023-07-28 17:15:53 +0200

    Second commit

--- app/src/main/java/com/mastercoding/helloworld2/MainActivityViewModel.java
+++ app/src/main/java/com/mastercoding/helloworld2/MainActivityViewModel.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel;
 
 public class MainActivityViewModel extends ViewModel {
     private int counter = 40;
+    // Hello we making first change
 
     private MutableLiveData<Integer> countLiveData = new MutableLiveData<>();
 
